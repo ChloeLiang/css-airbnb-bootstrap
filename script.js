099f15faa5b5d3403d0595d0f021b578330d774d
ChloeLiang <nliangxin@gmail.com>
2018-10-17 13:02:11 +0800

    add styles

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  document.querySelector('.btn-guidebooks').addEventListener('click', function () {
+    alert('This function isn\'t done yet');
+  });
+
+  document.querySelector('.btn-destinations').addEventListener('click', function () {
+    alert('This function isn\'t done yet');
+  });
+};
